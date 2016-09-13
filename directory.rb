4553c20fd72fa291c105e492c218f4afac626eb3
Dionysis <Dionysis.k22@gmail.com>
2016-09-13 16:07:54 +0000

    directory exercise 8.7

--- directory.rb
+++ directory.rb
@@ -1,22 +1,45 @@
 def input_students
-	puts "Please enter the name of the student and hit return"
-	puts "Then enter the year they were born and hit return"
-	puts "To finish, just hit return twice"
+	puts "Please enter the the information about the students and hit return"
 	# create an empty array
 	students = []
-	# get the first name
-	name = gets.chomp
-	yob = gets.chomp # year of birth
-	# while the name is not empty, repeat this code
-	while !name.empty? do	
-	# add the student hash to the array
-	students << {name: name, cohort: :november, year_of_birth: yob}
-	# get another name from the user
-	puts "Add another student with their year of birth"
-	name = gets.chomp
-	yob = gets.chomp
+	# creating a while loop that will add the information to the students array
+	# otherwise break the loop and remove the last student
+
+	loop do
+		# create a defaul student hash with the default values
+		# in case there is no input.
+		student_hash = {name: "Unknown detail", cohort: "Unknown detail", Year_of_birth: "Unknown detail"}
+		student_hash.each do |key, value|
+			puts "Enter #{key}"
+			input = gets.chomp.capitalize.to_sym # capitalizing the input from the user and converting symbol
+			if input != "".to_sym
+				student_hash[key] = input
+			else
+				student_hash[key]
+			end
+		end
+
+		students << student_hash # adding the student information hash to the students array
+		puts "------------"
+		# review the last student added
+		last_student = students.last
+		puts "Review last addition details:"
+		puts "Name: #{last_student[:name]} \nCohort: #{last_student[:cohort]} \nBorn in: #{last_student[:Year_of_birth]}"
+		puts ""
+		puts "You have the following options:"
+		puts "Type 'no' to delete the last student"
+		puts "Hit 'return' to add another student"
+		puts "Type 'exit' to finish adding students and show a list of all students"
+		puts ""
+		input2 = gets.chomp
+		if input2 == "no"
+			students.pop # removing the last student hash
+		elsif input2 == "exit"
+			break			
+		end
+
 	end
-	# return the array of students
+	# returning the students array
 	students
 end
 
@@ -31,7 +54,7 @@ def print(students)
 	counter = 0
 	while counter < students.length
 		student = students[counter]
-		puts "#{counter+1}. #{student[:name]} (#{student[:cohort]} cohort), born in is #{student[:year_of_birth]}".center(50)
+		puts "#{counter+1}. #{student[:name]} (#{student[:cohort]} cohort), born in #{student[:Year_of_birth]}".center(50)
 		counter += 1
 	end
 end

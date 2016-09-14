1c99c92b679b4da7b4fde5e044867e07dff127da
Dionysis <Dionysis.k22@gmail.com>
2016-09-14 14:25:49 +0000

    directory 10 refactoring the code

--- directory.rb
+++ directory.rb
@@ -1,32 +1,41 @@
+@students = [] # emty array accesible to all methods
+
 def interactive_menu
-	students = []
 	loop do
-	# 1. print the menu and ask the user what to do
-	puts "1. Inputs the students"
+	print_menu
+	process(gets.chomp)
+	end
+end
+
+def print_menu
+	puts "1. Input the students"
 	puts "2. Show the students"
-	puts "9. Exit"
-	# 2. read the input and save it to a variable
-	selection = gets.chomp
-	# 3. do what the user has asked 
-		case selection
-		when "1"
-			students = input_students
-		when "2"
-			print_header
-			print(students)
-			print_footer(students)
-		when "9"
-			exit
-		else
-			puts "I don't know what you meant, try again"
-		end
+	puts "9. Exit" #
+end
+
+def show_students
+	print_header
+	print_students_list(@students)
+	print_footer(@students)
+end
+
+def process(selection)
+	case selection
+	when "1"
+		input_students
+	when "2"
+		show_students
+	when "9"
+		exit
+	else
+		puts "I don't know what you mean, try again"
 	end
 end
 
 def input_students
 	puts "Please enter the the information about the students and hit return"
 	# create an empty array
-	students = []
+	#@students = []
 	# creating a while loop that will add the information to the students array
 	# otherwise break the loop and remove the last student
 
@@ -45,16 +54,16 @@ def input_students
 		end
 
 
-		students << student_hash # adding the student information hash to the students array
+		@students << student_hash # adding the student information hash to the students array
 		puts
-		if students.count == 1
+		if @students.count == 1
 			puts "Now he have 1 student"
 		else
-			puts "Now we have #{students.count} students"
+			puts "Now we have #{@students.count} students"
 		end
 		puts "------------"
 		# review the last student added
-		last_student = students.last
+		last_student = @students.last
 		puts "Review last addition details:"
 		puts "Name: #{last_student[:name]} \nCohort: #{last_student[:cohort]} \nBorn in: #{last_student[:Year_of_birth]}"
 		puts ""
@@ -65,7 +74,7 @@ def input_students
 		puts ""
 		input2 = gets[0..-2]
 		if input2 == "no"
-			students.pop # removing the last student hash
+			@students.pop # removing the last student hash
 		elsif input2 == "exit"
 			break			
 		end
@@ -73,7 +82,7 @@ def input_students
 	end
 
 	# returning the students array
-	students
+	@students
 
 	
 	# grouping cohorts
@@ -89,12 +98,12 @@ def print_header
 	puts "-------------".center(50)
 end
 
-def print(students)
+def print_students_list(students)
 	# students = the students array
 	# counter = index
 	counter = 0
-	while counter < students.length
-		student = students[counter]
+	while counter < @students.length
+		student = @students[counter]
 		puts "#{counter+1}. #{student[:name]} (#{student[:cohort]} cohort), born in #{student[:Year_of_birth]}".center(50)
 		counter += 1
 	end
@@ -106,10 +115,10 @@ end
 
 
 def print_footer(students)
-	if students.count == 1
+	if @students.count == 1
 	puts "Overall, we have 1 great student".center(50)
 	else
-	puts "Overall, we have #{students.count} great students".center(50)	
+	puts "Overall, we have #{@students.count} great students".center(50)	
 	end
 end
 

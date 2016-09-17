16d4679e6822da258da25cabae8bb22cb7dfc00a
Dionysis <Dionysis.k22@gmail.com>
2016-09-17 10:46:31 +0000

    adding feedback messages

--- directory.rb
+++ directory.rb
@@ -27,11 +27,13 @@ def interactive_menu
 end
 
 def print_menu
+  puts
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
   puts "4. Load the list from students.csv"
   puts "9. Exit" # 9 because we'll be adding more items
+  puts
 end
 
 def show_students
@@ -43,14 +45,29 @@ end
 def process(selection)
   case selection
   when "1"
+    puts "Accepted input:"
+    puts "Input the students"
+    puts
     input_students
   when "2"
+    puts "Accepted input:"
+    puts "Show the students"
+    puts
     show_students
   when "3"
-    save_students  
+    puts "Accepted input"
+    puts "Save the list to students.csv"
+    puts
+    save_students
   when "4"
+    puts "Accepted input"
+    puts "Load the list from students.csv"
+    puts
     try_load_students
   when "9"
+    puts "Accepted input"
+    puts "Exit"
+    puts
     exit # this will cause the program to terminate
   else
     puts "I don't know what you meant, try again"
@@ -90,6 +107,7 @@ def try_load_students
 	if File.exists?(filename) # if it exists
 		load_students(filename)
 		puts "Loaded #{@students.count} from #{filename}"
+    puts
 	else #if it doesn't exist
 	puts "Sorry, #{filename} doesn't exist."
 	exit # quit the program
